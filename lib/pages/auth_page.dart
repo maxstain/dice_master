d9282cf14042f417175c81542a078b68936da1fd
Firas Chabchoub <firaschabchoub@hotmail.com>
2025-08-11 19:57:25 +0100

    Working on the auth pages.

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,5 @@
 import 'package:dice_master/pages/homepage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -11,45 +12,7 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
-    late bool isLoginScreen = true; // This can be toggled based on your logic
-
-    if (isLoginScreen) {
-      return Scaffold(
-        appBar: AppBar(title: const Text('Login')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Login Screen'),
-              ElevatedButton(
-                onPressed: () {
-                  // Handle login logic
-                },
-                child: const Text('Login'),
-              ),
-            ],
-          ),
-        ),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(title: const Text('Register')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Register Screen'),
-              ElevatedButton(
-                onPressed: () {
-                  // Handle registration logic
-                },
-                child: const Text('Register'),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
+    return LoginPage();
   }
 }
 
@@ -64,5 +27,107 @@ class AuthWrapper extends StatelessWidget {
     return !isAuthenticated
         ? const AuthPage()
         : const MyHomePage(title: "Dice Master");
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Login Screen'),
+            ElevatedButton(
+              onPressed: () {
+                // Handle login logic
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        const MyHomePage(title: "Dice Master"),
+                  ),
+                );
+              },
+              child: const Text('Login'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Switch to Register Screen
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegisterPage()),
+                );
+                setState(() {
+                  if (kDebugMode) {
+                    print('Switched to Register Screen');
+                  }
+                });
+              },
+              child: const Text('Register instead'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Register Screen'),
+            ElevatedButton(
+              onPressed: () {
+                // Handle registration logic
+                setState(() {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                });
+              },
+              child: const Text('Register'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  if (kDebugMode) {
+                    print('Switched to Login Screen');
+                  }
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                });
+              },
+              child: const Text('Login instead'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:dice_master/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -21,6 +22,43 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.deepPurple),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Handle item tap
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Handle item tap
+              },
+            ),
+            ListTile(
+              trailing: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                // Handle logout
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SplashScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
