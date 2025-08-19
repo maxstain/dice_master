1d130662b1630c08e44a475dc4af34f46518a6b4
Firas Chabchoub <firaschabchoub@hotmail.com>
2025-08-19 20:09:42 +0100

    Implementing Firebase.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,18 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:dice_master/features/splash/splash_screen.dart';
 import 'package:dice_master/main.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Splash Screen test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const DiceMasterApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.pumpAndSettle(); // Wait for animations to complete
+    // Verify that the splash screen is displayed
+    expect(find.byType(SplashScreen), findsOneWidget);
+    // Verify that the Lottie animation is present
+    expect(find.byType(Lottie), findsOneWidget);
+    // Verify that the splash screen has a specific text
+    expect(find.text('Dice Master'), findsOneWidget);
   });
 }
